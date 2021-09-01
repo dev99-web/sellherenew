b0f5f1e5e2cc616bc29651c6c36aabadd5b5b5d0
dev99-web <87270269+dev99-web@users.noreply.github.com>
2021-09-01 23:20:17 +0530

    MainProject

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './Header.css';
+import OlxLogo from '../../assets/OlxLogo';
+import Search from '../../assets/Search';
+import Arrow from '../../assets/Arrow';
+import SellButton from '../../assets/SellButton';
+import SellButtonPlus from '../../assets/SellButtonPlus';
+function Header() {
+  return (
+    <div className="headerParentDiv">
+      <div className="headerChildDiv">
+        <div className="brandName">
+        <h1><span className="span1">Sell</span><span>Here</span></h1>
+        </div>
+        <div className="placeSearch">
+          <Search></Search>
+          <input type="text" />
+          <Arrow></Arrow>
+        </div>
+        <div className="productSearch">
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Find car,mobile phone and more..."
+            />
+          </div>
+          <div className="searchAction">
+            <Search color="#ffffff"></Search>
+          </div>
+        </div>
+        
+        <div className="loginPage">
+          <span>Login</span>
+          <hr />
+        </div>
+
+        <div className="sellMenu">
+          <SellButton></SellButton>
+          <div className="sellMenuContent">
+            <SellButtonPlus></SellButtonPlus>
+            <span>SellHere</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Posts/Posts.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import Heart from '../../assets/Heart';
+import './Post.css';
+
+function Posts() {
+
+  return (
+    <div className="postParentDiv">
+      <div className="moreView">
+        <div className="heading">
+          <span>Quick Menu</span>
+          
+        </div>
+        <div className="cards">
+          <div
+            className="card"
+          >
+          
+            <div className="image">
+              <img src="https://th.bing.com/th/id/OIP.C1SbY5OTtEj7pi-SuGPXogHaDt?pid=ImgDet&rs=1" alt="" />
+            </div>
+            <div className="content">
+              <p className="rate">&#x20B9; 250000</p>
+              <span className="kilometer">Two Wheeler</span>
+              <p className="name"> YAMAHA R15V3</p>
+            </div>
+            <div className="date">
+              <span>Tue May 04 2021</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="recommendations">
+        <div className="heading">
+          <span>Fresh recommendations</span>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="favorite">
+              <Heart></Heart>
+            </div>
+            <div className="image">
+              <img src="../../../Images/R15V3.jpg" alt="" />
+            </div>
+            <div className="content">
+              <p className="rate">&#x20B9; 250000</p>
+              <span className="kilometer">Two Wheeler</span>
+              <p className="name"> YAMAHA R15V3</p>
+            </div>
+            <div className="date">
+              <span>10/5/2021</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Posts;
